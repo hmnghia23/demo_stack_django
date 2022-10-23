37fa6e955cdc717cd8b0433612c2809768212875
hmnghia23 <honghia06@gmail.com>
2022-10-23 22:08:47 +0700

    update

--- myproject/env.py
+++ myproject/env.py
@@ -9,11 +9,11 @@ class AppEnvironment(BaseSettings):
     APP_POSTGRES_HOST: str 
     APP_POSTGRES_PORT: str
 
-    APP_MYSQL_USERNAME: str
-    APP_MYSQL_PASSWORD: str
-    APP_MYSQL_DB: str
-    APP_MYSQL_HOST: str 
-    APP_MYSQL_PORT: str
+    # APP_MYSQL_USERNAME: str
+    # APP_MYSQL_PASSWORD: str
+    # APP_MYSQL_DB: str
+    # APP_MYSQL_HOST: str 
+    # APP_MYSQL_PORT: str
 
     APP_REDIS_PASSWORD: str
     APP_REDIS_HOST: str
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env.APP_POSTGRES_DB,
-#         'USER': env.APP_POSTGRES_USERNAME,
-#         'PASSWORD': env.APP_POSTGRES_PASSWORD,
-#         'HOST': env.APP_POSTGRES_HOST,        
-#         'PORT': env.APP_POSTGRES_PORT,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.APP_POSTGRES_DB,
+        'USER': env.APP_POSTGRES_USERNAME,
+        'PASSWORD': env.APP_POSTGRES_PASSWORD,
+        'HOST': env.APP_POSTGRES_HOST,        
+        'PORT': env.APP_POSTGRES_PORT,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
